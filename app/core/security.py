1924175b29abbe6916950ddd9de322c08ac0278f
Freeluncher <gandhisatriadewa06@gmail.com>
2025-05-12 05:24:48 +0700

    menambahkan user admin baru

--- app/core/security.py
+++ app/core/security.py
@@ -3,7 +3,7 @@ from jose import jwt
 from passlib.context import CryptContext
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
